79b68e21855efbea357495dcba7b9786935e7c07
hittoxkarkor <hitx@hotmail.Fr>
2019-01-26 11:41:20 +0100

    "Modification site.css"

--- e_learning/site/tpl/header.php
+++ e_learning/site/tpl/header.php
@@ -2,11 +2,19 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" type="text/css" href="./www/bootstrap/bootstrap.min.css">		
+        <link rel="stylesheet" type="text/css" href="./www/bootstrap/bootstrap.min.css">	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
         <link rel="stylesheet" type="text/css" href="./www/css/style.css">
+		<link rel="stylesheet" type="text/css" href="./www/css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="./www/css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="./www/css/component.css" />
 		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-		<script type="text/javascript" src="./www/js/app.js"></script>		
+		<script type="text/javascript" src="./www/js/app.js"></script>	
+		<script src="./www/js/modernizr.custom.js"></script>	
         <title><?php echo $tpl_var['titre_page'] ?></title>
+		<meta name="keywords" content="google nexus 7 menu, css transitions, sidebar, side menu, slide out menu" />
+		<meta name="author" content="Codrops" /> 			
+		<link rel="shortcut icon" href="../favicon.ico">
     </head>
 
     <body>
